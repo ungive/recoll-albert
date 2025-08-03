747e7b6dcb6086df09bbc7ff7487f783bcccf29d
Jonas van den Berg <jonas@vandenbe.com>
2025-08-03 19:50:31 +0200

    Fix plugin

--- __init__.py
+++ __init__.py
@@ -1,233 +1,306 @@
-"""Recoll."""
-import os
-from sys import platform
-import traceback
+# -*- coding: utf-8 -*-
+from albert import *
 from pathlib import Path
+from sys import platform
 from collections import Counter
+import os
+import traceback
 
-# TODO: Check if this fails and return a more comprehensive error message for the user
+import sys
+sys.path.append('/usr/local/lib64/python3.13/site-packages')
+
 from recoll import recoll
 
-import albertv0 as v0
+md_iid = "3.0"
+md_version = "1.0"
+md_name = "Recoll"
+md_description = "Search Recoll index"
+md_license = "MIT"
+md_url = "https://github.com/gerardsimons/recoll-albert"
+md_authors = "Gerard Simons"
 
-__iid__ = "PythonInterface/v0.2"
-__prettyname__ = "Recoll"
-__version__ = "0.1.0"
-# __trigger__ = "rc " # Use this if you want it to be only triggered by rc <query>
-__author__ = "Gerard Simons"
-__dependencies__ = []
-__homepage__ = "https://github.com/gerardsimons/recoll-albert/blob/master/recoll/recoll"
+config = {
+    # Files are searched immediately when opening Albert.
+    # This will make it impossible to use other actions.
+    'always_search_files': True,
+    'reveal-file-command': {
+        # FIXME This is only for nautilus.
+        'linux': ['nautilus', '--new-window']
+    }
+}
 
-icon_path = str(Path(__file__).parent / "recoll")
-cache_path = Path(v0.cacheLocation()) / "recoll"
-config_path = Path(v0.configLocation()) / "recoll"
-data_path = Path(v0.dataLocation()) / "recoll"
-dev_mode = True
-
-# If set to to true it removes duplicate documents hits that share the same URL, such as epubs or other archives.
-# Note that this means the number of results returned may be different from the actual recoll results
+icon_path = str(Path(__file__).parent / "recoll.svg")
 remove_duplicates = True
 
-# String definitions
-OPEN_WITH_DEFAULT_APP = "Open with default application"
-REVEAL_IN_FILE_BROWSER = "Reveal in file browser"
-OPEN_TERMINAL_AT_THIS_PATH = "Open terminal at this path"
-COPY_FILE_CLIPBOARD = "Copy file to clipboard"
-COPY_PATH_CLIPBOARD = "Copy path to clipboard"
-
-# plugin main functions -----------------------------------------------------------------------
-
-def initialize():
-    """Called when the extension is loaded (ticked in the settings) - blocking."""
-
-    # create plugin locations
-    for p in (cache_path, config_path, data_path):
-        p.mkdir(parents=False, exist_ok=True)
-
-def query_recoll(query_str, max_results=10, max_chars=80, context_words=4, verbose=False):
+def extract_around_match(query: str, abstract: str, N: int = 80) -> str:
     """
-    Query recoll index for simple query string and return the filenames in order of relevancy
-    :param query_str:
-    :param max_results:
-    :return:
+    Find the substring of abstract best matching the query and return up to N chars 
+    centered on the entire matching substring (not just start).
+
+    Args:
+      query (str): the search query
+      abstract (str): text to search in
+      N (int): length of snippet around the best match
+
+    Returns:
+      str: snippet of abstract around best match
     """
-    if not query_str:
-        return []
 
-    db = recoll.connect()
-    db.setAbstractParams(maxchars=max_chars, contextwords=context_words)
-    query = db.query()
-    nres = query.execute(query_str)
-    docs = []
-    if nres > max_results:
-        nres = max_results
-    for i in range(nres):
-        doc = query.fetchone() # TODO: JUst use fetchall and return the lot?!
-        docs.append(doc)
+    query = query.lower()
+    abstract_lower = abstract.lower()
 
-    return docs
+    qlen = len(query)
+    alen = len(abstract)
+
+    if qlen == 0 or alen == 0:
+        return ""
+
+    window_size = qlen
+    best_pos = 0
+    best_score = -1
+
+    for i in range(alen - window_size + 1):
+        window = abstract_lower[i:i+window_size]
+        score = sum(1 for a, b in zip(window, query) if a == b)
+        if score > best_score:
+            best_score = score
+            best_pos = i
+
+    # Center snippet around the middle of the matched substring
+    match_center = best_pos + (qlen // 2)
+    half_len = N // 2
+
+    start = max(0, match_center - half_len)
+    end = start + N
+
+    # Adjust if snippet goes past the abstract length
+    if end > alen:
+        end = alen
+        start = max(0, end - N)
+
+    snippet = abstract[start:end]
+
+    prefix = "..." if start > 0 else ""
+    suffix = "..." if end < alen else ""
+
+    return prefix + snippet + suffix
+
+def replace_home_with_tilde(path: str) -> str:
+    home = os.path.expanduser("~")
+    if path.startswith(home):
+        return "~" + path[len(home):]
+    return path
+    
+from pathlib import Path
+
+def find_system_mime_icon(mime_type: str):
+    icon_name = mime_type.replace('/', '-')
+    sizes = [32, 36, 48, 64, 72, 96, 128, 192, 256]
+    icon_dirs = [
+        '/usr/share/icons/breeze',
+    ]
+    extensions = ['.png', '.svg', '.xpm']
+
+    for icon_dir in icon_dirs:
+        for size in sizes:
+            size_str = f"{size}x{size}"
+
+            # First try: /size/mimetypes/icon_name.ext
+            for ext in extensions:
+                path1 = Path(icon_dir) / size_str / 'mimetypes' / (icon_name + ext)
+                if path1.is_file():
+                    return str(path1)
+
+            # Fallback try: /mimetypes/size/icon_name.ext
+            for ext in extensions:
+                path2 = Path(icon_dir) / 'mimetypes' / str(size) / (icon_name + ext)
+                if path2.is_file():
+                    return str(path2)
+
+        # Also fallback to mimetypes folder root without size
+        for ext in extensions:
+            path3 = Path(icon_dir) / 'mimetypes' / (icon_name + ext)
+            if path3.is_file():
+                return str(path3)
+
+    return None
 
 
-def path_from_url(url: str) -> str:
-    if not url.startswith('file://'):
-        return None
-    else:
+
+class Plugin(PluginInstance, TriggerQueryHandler):
+
+    def __init__(self):
+        PluginInstance.__init__(self)
+        TriggerQueryHandler.__init__(self)
+        
+        self.cache_path = Path(self.cacheLocation()) / "recoll"
+        self.config_path = Path(self.configLocation()) / "recoll"
+        self.data_path = Path(self.dataLocation()) / "recoll"
+
+        for p in (self.cache_path, self.config_path, self.data_path):
+            p.mkdir(parents=True, exist_ok=True)
+
+    def defaultTrigger(self):
+        return "" if config['always_search_files'] else "rc "
+
+    def query_rec(self, query_str, max_results=10, max_chars=80, context_words=1):
+        if not recoll:
+            return []
+
+        if not query_str:
+            return []
+            
+        query_str = f'{query_str}*'
+
+        try:
+            db = recoll.connect()
+            db.setAbstractParams(maxchars=max_chars, contextwords=context_words)
+            query = db.query()
+            nres = query.execute(query_str)
+            if nres > max_results:
+                nres = max_results
+            docs = [query.fetchone() for _ in range(nres)]
+            # print([d.abstract for d in docs])
+            return docs
+        except Exception:
+            if __debug__:
+                traceback.print_exc()
+            return []
+
+    def remove_duplicate_docs(self, docs):
+        urls = [x.url for x in docs]
+        url_count = Counter(urls)
+
+        duplicates = [k for k in url_count if url_count[k] > 1]
+
+        for dup in duplicates:
+            best_doc = None
+            best_rating = -1
+            for doc in [x for x in docs if x.url == dup]:
+                rating = float(doc.relevancyrating.replace("%", ""))
+                if rating > best_rating:
+                    best_doc = doc
+                    best_rating = rating
+            docs = [x for x in docs if x.url != dup]
+            docs.append(best_doc)
+        return docs
+
+    def path_from_url(self, url: str) -> str:
+        if not url.startswith("file://"):
+            return None
         return url.replace("file://", "")
 
+    def get_reveal_file_action(self, dir_path: str, file_path: str):
+        if sys.platform.startswith("linux"):
+            return Action(
+                id="reveal_in_file_browser",
+                text="Reveal in file browser",
+                callable=lambda: runDetachedProcess([*config['reveal-file-command']['linux'], file_path])
+            )
+        elif sys.platform == "darwin":
+            # FIXME doesnt reveal the file
+            return Action(
+                id="reveal_in_file_browser",
+                text="Reveal in file browser",
+                callable=lambda: runDetachedProcess(['open', dir_path])
+            )
+        elif sys.platform == "win32":
+            # FIXME doesnt reveal the file
+            return Action(
+                id="reveal_in_file_browser",
+                text="Reveal in file browser",
+                callable=lambda: os.startfile(dir_path)
+            )
+        else:
+            return None
 
-def get_open_dir_action(dir: str):
-    if platform == "linux" or platform == "linux2":
-        return v0.ProcAction(text=REVEAL_IN_FILE_BROWSER, commandline=["xdg-open", dir])
-    elif platform == "darwin":
-        return v0.ProcAction(text=REVEAL_IN_FILE_BROWSER, commandline=["open", dir])
-    elif platform == "win32": # From https://stackoverflow.com/a/2878744/916382
-        return v0.FuncAction(text=REVEAL_IN_FILE_BROWSER, callable=lambda : os.startfile(dir))
+    def doc_to_icon_path(self, doc):
+        mime_str = getattr(doc, "mtype", None)
+        if not mime_str:
+            return icon_path
+        mime_str = mime_str.replace("/", "-")
+        icon_p = find_system_mime_icon(mime_str)
+        if not icon_p:
+            icon_p = find_system_mime_icon("text-plain")
+        if not icon_p:
+            icon_p = icon_path
+        return icon_p
 
+    def recoll_docs_as_items(self, docs, query):
+        items = []
+        if remove_duplicates:
+            docs = self.remove_duplicate_docs(docs)
 
-def doc_to_icon_path(doc) -> str:
-    """ Attempts to convert a mime type to a text string that is accepted by """
-    mime_str = getattr(doc, "mtype", None)
-    if not mime_str:
-        return v0.iconLookup("unknown")
-    mime_str = mime_str.replace("/", "-")
-    icon_path = v0.iconLookup(mime_str)
-    if not icon_path:
-        icon_path = v0.iconLookup("unknown")
-    return icon_path
+        for doc in docs:
+            path = self.path_from_url(doc.url)
+            if not path:
+                continue
+            dir_path = os.path.dirname(path)
+            dir_open = self.get_reveal_file_action(dir_path, path)
 
-
-def remove_duplicate_docs(docs: list):
-    """
-    Removes Recoll docs that have the same URL but actually refer to different files, for example an epub file
-    which contains HTML files will have multiple docs for each but they all refer to the same epub file.
-    :param docs: the original list of documents
-    :return: the same docs param but with the docs removed that share the same URL attribute
-    """
-    urls = [x.url for x in docs]
-    url_count = Counter(urls)
-
-    duplicates = [k for k in url_count.keys() if url_count[k] > 1]
-    # Merge duplicate results, this might happen becase it actually consists of more than 1 file, like an epub
-    # We adopt the relevancy rating of the max one
-    for dup in duplicates:
-        # Just take the one with the highest relevancy
-        best_doc = None
-        best_rating = -1
-        for doc in [x for x in docs if x.url == dup]:
-            rating = float(doc.relevancyrating.replace("%", ""))
-            if rating > best_rating:
-                best_doc = doc
-                best_rating = rating
-
-        docs = [x for x in docs if x.url != dup]
-        docs.append(best_doc)
-    return docs
-
-def recoll_docs_as_items(docs: list):
-    """Return an item - ready to be appended to the items list and be rendered by Albert."""
-
-    items = []
-
-    # First we find duplicates if so configured
-    if remove_duplicates:
-        docs = remove_duplicate_docs(docs)
-
-    for doc in docs:
-        path = path_from_url(doc.url) # The path is not always given as an attribute by recoll doc
-        dir = os.path.dirname(path)
-        dir_open = get_open_dir_action(dir)
-
-        if path:
-            actions = [v0.UrlAction(OPEN_WITH_DEFAULT_APP, doc.url)]
+            # FIXME Albert does not show any actions and only starts the first one with Enter.
+            actions = []
             if dir_open:
                 actions.append(dir_open)
+            actions.append(Action("open_with_default_application", "Open with default application", lambda u=doc.url: openUrl(u)))
+                
+            abstract = doc.abstract
+            abstract = extract_around_match(query, abstract, 80)
 
+            # FIXME
+            """
             actions.extend([
-                # NOTE: Termaction requires a commandline arg, if you pass it empty list nothing happens, so we give it empty string
-                v0.TermAction(text=OPEN_TERMINAL_AT_THIS_PATH, commandline=[""],
-                          behavior=v0.TermAction.CloseBehavior.DoNotClose, cwd=dir),
-                # IMPORTANT: FIXME: it should be only read when the user clicks it I feel. A lot of IO could happen just to create these actions
-                v0.ClipAction(COPY_FILE_CLIPBOARD, open(path, 'rb').read()),
-                v0.ClipAction(COPY_PATH_CLIPBOARD, path)])
+                TermAction(
+                    text="Open terminal at this path",
+                    commandline=[""],
+                    behavior=TermAction.CloseBehavior.DoNotClose,
+                    cwd=dir_path
+                ),
+                ClipAction("Copy file to clipboard", open(path, 'rb').read()),
+                ClipAction("Copy path to clipboard", path),
+            ])
+            """
 
-            # Add the item
-            items.append(v0.Item(
-                id=__prettyname__,
-                icon=doc_to_icon_path(doc),
-                text=doc.filename,
-                subtext=dir,
-                completion="",
-                actions=actions
-            ))
-    return items
+            items.append(
+                StandardItem(
+                    id=self.id(),
+                    iconUrls=[self.doc_to_icon_path(doc)],
+                    text=f"{doc.filename} • {replace_home_with_tilde(dir_path)}",
+                    subtext=abstract,
+                    actions=actions
+                )
+            )
+        return items
 
+    def handleTriggerQuery(self, query):
+        if not recoll:
+            query.add(
+                StandardItem(
+                    id=self.id(),
+                    iconUrls=[icon_path],
+                    text="Recoll Python module not found",
+                    subtext="Make sure Recoll Python bindings are installed and in your PYTHONPATH",
+                )
+            )
+            return
 
-def handleQuery(query) -> list:
-    """Hook that is called by albert with *every new keypress*."""  # noqa
-    results = []
+        if not query.string.strip():
+            return
 
-    try:
-        if __trigger__  and not query.isTriggered:
-            return []
-        # be backwards compatible with v0.2
-        if "disableSort" in dir(query):
-            query.disableSort()
-
-        results_setup = setup(query)
-        if results_setup:
-            return results_setup
-
-        docs = query_recoll(query.string)
-        results = recoll_docs_as_items(docs)
-    except Exception:  # user to report error
-        if dev_mode:  # let exceptions fly!
-            print(traceback.format_exc())
-            raise
-
-        results.insert(
-            0,
-            v0.Item(
-                id=__prettyname__,
-                icon=icon_path,
-                text="Something went wrong! Press [ENTER] to copy error and report it",
-                actions=[
-                    v0.ClipAction(
-                        f"Copy error - report it to {__homepage__[8:]}",
-                        f"{traceback.format_exc()}",
-                    )
-                ],
-            ),
-        )
-
-    return results
-
-
-# supplementary functions ---------------------------------------------------------------------
-def save_data(data: str, data_name: str):
-    """Save a piece of data in the configuration directory."""
-    with open(config_path / data_name, "w") as f:
-        f.write(data)
-
-
-def load_data(data_name) -> str:
-    """Load a piece of data from the configuration directory."""
-    with open(config_path / data_name, "r") as f:
-        data = f.readline().strip().split()[0]
-
-    return data
-
-
-def setup(query):
-    """Setup is successful if an empty list is returned.
-
-    Use this function if you need the user to provide you data
-    """
-
-    results = []
-    return results
-
-# In case the __trigger__ was not set at all we set it to the empty string
-try:
-    __trigger__
-except NameError:
-    __trigger__ = ""
+        try:
+            qs = query.string.strip()
+            docs = self.query_rec(qs)
+            items = self.recoll_docs_as_items(docs, qs)
+            for item in items:
+                query.add(item)
+        except Exception:
+            if __debug__:
+                traceback.print_exc()
+            query.add(
+                StandardItem(
+                    id=self.id(),
+                    iconUrls=[icon_path],
+                    text="Error querying Recoll",
+                    subtext="Check logs for details",
+                )
+            )
